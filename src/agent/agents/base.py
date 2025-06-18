d66ab6b75ccf09343946ce9ba54dcdc8ee84bf9e
John Wei <wzy1048168235@163.com>
2025-06-18 15:17:13 +0800

    refactor(main): 删除GraphAgent

--- src/agent/agents/base.py
+++ src/agent/agents/base.py
@@ -1,17 +1,14 @@
 # -*- coding: UTF-8 -*-
-from abc import ABC, abstractmethod
+from abc import ABC
 from contextlib import AsyncExitStack
 
 from fastmcp import Client as MCPClient
 from fastmcp.client.transports import MCPConfigTransport
 from icecream import ic
 from langchain_core.messages import HumanMessage
-from langgraph.pregel import Pregel
-from pydantic import BaseModel
 
 from config import Configuration
 from llm import LLMFactory
-from workflow import GraphBuilder, GraphConfig
 
 
 class BaseAgent(ABC):
@@ -47,26 +44,3 @@ class BaseAgent(ABC):
         except Exception as e:
             ic(e)
 
-
-class GraphAgent(BaseAgent):
-    state_schema: type[BaseModel]
-    input_schema: type[BaseModel] | None = None
-    output_schema: type[BaseModel] | None = None
-
-    def __init__(self, config: Configuration):
-        super().__init__(config)
-        self.workflow: Pregel | None = None
-
-    async def initialize(self):
-        await super().initialize()
-        if self.workflow is not None:
-            raise RuntimeError(f"Client already initialized")
-        # build graph
-        builder = GraphBuilder(self.state_schema, self.input_schema, self.output_schema)
-        graph_config = self._get_workflow()
-        builder.from_config(graph_config)
-        self.workflow = builder.compile()
-
-    @abstractmethod
-    def _get_workflow(self) -> GraphConfig:
-        ...
